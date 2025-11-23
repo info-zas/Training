cdcf25a89b0c1a4039bbad5f36ee22beabef82d3
Fakruddin Mohammed <info@zas-robotics.com>
2025-11-23 22:57:27 +0000

    added a new file

--- GPREC/05_Navigational_Robot_Car-2-Comms/01_Code/01_Car2_Basic_Movements/01_Car2_Basic_Movements.ino
+++ GPREC/05_Navigational_Robot_Car-2-Comms/01_Code/01_Car2_Basic_Movements/01_Car2_Basic_Movements.ino
@@ -1,93 +1,88 @@
-#include <Wire.h>
-#include <QMC5883LCompass.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#define Left1 7
+#define Left2 8
+#define Left_Speed 6
 
-// --- OLED ---
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-#define OLED_RESET -1
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-// --- Magnetometer ---
-QMC5883LCompass compass;
-
-// --- Calibration (runtime min/max) ---
-int16_t xMin = 32767, xMax = -32768;
-int16_t yMin = 32767, yMax = -32768;
-int16_t zMin = 32767, zMax = -32768;
+#define Right1 3
+#define Right2 4
+#define Right_Speed 5
 
 void setup() {
-  Serial.begin(9600);
-  Wire.begin();
+  pinMode(Left1, OUTPUT);
+  pinMode(Left2,OUTPUT);
+  pinMode(Left_Speed,OUTPUT); //pwm
 
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    // If this fails, reduce what you print or switch to U8g2 low-memory mode
-    while (1);
-  }
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0,0);
-  display.println(F("QMC5883L Calib"));
-  display.println(F("Rotate in all axes"));
-  display.display();
+  pinMode(Right1, OUTPUT);
+  pinMode(Right2,OUTPUT);
+  pinMode(Right_Speed,OUTPUT); //pwm
 
-  compass.init();
-  delay(800);
 }
 
 void loop() {
-  compass.read();
-  int x = compass.getX();
-  int y = compass.getY();
-  int z = compass.getZ();
+  analogWrite(Left_Speed,150);
+  analogWrite(Right_Speed,150);
 
-  // --- Update min/max ---
-  if (x < xMin) xMin = x; if (x > xMax) xMax = x;
-  if (y < yMin) yMin = y; if (y > yMax) yMax = y;
-  if (z < zMin) zMin = z; if (z > zMax) zMax = z;
-
-  // --- Offsets (hard-iron) ---
-  float xOff = (xMax + xMin) * 0.5f;
-  float yOff = (yMax + yMin) * 0.5f;
-  float zOff = (zMax + zMin) * 0.5f;
-
-  // --- Scales (soft-iron) ---
-  float xScale = (xMax - xMin) * 0.5f;
-  float yScale = (yMax - yMin) * 0.5f;
-  float zScale = (zMax - zMin) * 0.5f;
-
-  // Use averaged scale to keep math light on Uno
-  float avgScale = (xScale + yScale + zScale) / 3.0f;
-  if (avgScale < 1.0f) avgScale = 1.0f;  // safety
-
-  // --- Calibrated & normalized readings ---
-  float xCal = (x - xOff) / avgScale;
-  float yCal = (y - yOff) / avgScale;
-  // zCal not needed for 2D heading on a level surface
-
-  // --- Bearing from calibrated X/Y ---
-  float bearing = atan2f(yCal, xCal) * 180.0f / PI;
-  if (bearing < 0) bearing += 360.0f;
-
-  // --- OLED output (compact) ---
-  display.clearDisplay();
-  display.setCursor(0,0);
-  display.print(F("Bear: ")); display.print(bearing, 1); display.println(F(" deg"));
-
-  // Show calibrated values briefly (helps verify offsets)
-  display.print(F("Xc:")); display.print(xCal, 2);
-  display.print(F(" Yc:")); display.println(yCal, 2);
-
-  display.print(F("Xmn:")); display.print(xMin);
-  display.print(F(" Xmx:")); display.println(xMax);
-  display.print(F("Ymn:")); display.print(yMin);
-  display.print(F(" Ymx:")); display.println(yMax);
-  display.print(F("Zmn:")); display.print(zMin);
-  display.print(F(" Zmx:")); display.println(zMax);
-
-  display.display();
-
-  delay(250);
+  forward();
+  delay(2000);
+  backward();
+  delay(2000);
+  turnRight();
+  delay(500);
+  turnLeft();
+  //stpLeft();
+  delay(500);
+  Stop();
+  delay(1500);
+  //stpLeft();
+  //delay(500);*/
 }
+
+void forward(){  //forword
+digitalWrite(Left1, HIGH);  
+digitalWrite(Left2, LOW);  
+digitalWrite(Right1, HIGH);  
+digitalWrite(Right2, LOW); 
+}
+
+void backward(){  //backward
+digitalWrite(Left1, LOW);  
+digitalWrite(Left2, HIGH);  
+digitalWrite(Right1, LOW);  
+digitalWrite(Right2, HIGH); 
+}
+
+void turnRight(){ //turnRight
+digitalWrite(Left1, HIGH);  
+digitalWrite(Left2, LOW);  
+digitalWrite(Right1, LOW);  
+digitalWrite(Right2, HIGH); 
+}
+
+void stpRight() { 
+  digitalWrite(Left1, HIGH); 
+  digitalWrite(Left2, LOW);
+  digitalWrite(Right1, LOW);   
+  digitalWrite(Right2, LOW); 
+}
+
+void turnLeft(){ //turnLeft
+digitalWrite(Left1, LOW);  
+digitalWrite(Left2, HIGH);  
+digitalWrite(Right1, HIGH);  
+digitalWrite(Right2, LOW); 
+}
+
+void stpLeft() { 
+  digitalWrite(Left1, LOW); 
+  digitalWrite(Left2, LOW);
+  digitalWrite(Right1, HIGH);   
+  digitalWrite(Right2, LOW); 
+}
+
+void Stop(){ //turnLeft
+digitalWrite(Left1, LOW);  
+digitalWrite(Left2, LOW);  
+digitalWrite(Right1, LOW);  
+digitalWrite(Right2, LOW); 
+}
+
+
