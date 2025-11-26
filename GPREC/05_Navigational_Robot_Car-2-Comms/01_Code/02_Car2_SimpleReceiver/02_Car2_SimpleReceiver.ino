c5d7df491b57a34d290e1a925a35d6258d63dd50
info-zas <info@zas-robotics.com>
2025-11-26 21:46:22 +0000

    Update 02_Car2_SimpleReceiver.ino

--- GPREC/05_Navigational_Robot_Car-2-Comms/01_Code/02_Car2_SimpleReceiver/02_Car2_SimpleReceiver.ino
+++ GPREC/05_Navigational_Robot_Car-2-Comms/01_Code/02_Car2_SimpleReceiver/02_Car2_SimpleReceiver.ino
@@ -1,6 +1,17 @@
 #include <SPI.h>
 #include <RF24.h>
 
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+// OLED config
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 // -------------------- Motor Pins (given) --------------------
 #define IN1 7   // Left motor forward
 #define IN2 8   // Left motor backward
@@ -74,6 +85,19 @@ void right() {
 void setup() {
   Serial.begin(9600);
 
+   /// SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+   display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("System Initialized");
+  display.display();
+
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(ENA, OUTPUT);
@@ -102,15 +126,30 @@ void loop() {
     Serial.println(cmd);
 
     if (cmd.indexOf("up") == 0) {
+      showText("forward");
       forward();
     } else if (cmd.indexOf("down") == 0) {
+      showText("backward");
       backward();
     } else if (cmd.indexOf("left") == 0) {
+      showText("left");
       left();
     } else if (cmd.indexOf("right") == 0) {
+      showText("right");
       right();
     } else {
+      showText("stop");
       stopMotors();
     }
   }
+}
+
+void showText(const char* text) {
+    Serial.println(text);
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("Current Mode:");
+    display.println(text);
+    display.display();
+  
 }
